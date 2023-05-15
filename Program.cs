7b3dbde54ea5431e0688a262f4076f2ad8aa60a5
Mankar123sayu <125425650+Mankar123sayu@users.noreply.github.com>
2023-05-15 21:19:31 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,19 +6,6 @@ namespace FunctionalProblem
     {
         public static void Main(string[] args)
         {
-
-        // FlipCoin calculation = new FlipCoin();
-         //calculation.Flip();
-
-        //LeapYear leapYear = new LeapYear();
-        //leapYear.Leap();
-
-        // PowerofTwo powerofTwo = new PowerofTwo();
-         //powerofTwo.Power();
-
-        // HarmonicNumber harmonicNumber = new HarmonicNumber();
-        // harmonicNumber.Harmonic();
-
          Factors factors = new Factors();
          factors.Calculate();
         }
